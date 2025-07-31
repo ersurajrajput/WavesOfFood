de0e616d9dcead16c969c393f05ab070d8eb35fd
ersurajrajput <thsurajsinghrajput@gmail.com>
2025-07-31 11:19:58 +0530

    login screen added

---
+++ app/src/main/java/com/example/wavesoffood/SplashActivity.kt
@@ -0,0 +1,40 @@
+package com.example.wavesoffood
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.wavesoffood.ui.user.LoginActivity
+import com.example.wavesoffood.ui.user.OnBoardingActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Fade-in animation for el1
+            val intent = Intent(applicationContext,OnBoardingActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+
+
+
+
+    }
+
+}
--- app/src/main/java/com/example/wavesoffood/ui/user/OnBoardingActivity.kt
+++ app/src/main/java/com/example/wavesoffood/ui/user/OnBoardingActivity.kt
@@ -1,20 +1,30 @@
-package com.example.wavesoffood
+package com.example.wavesoffood.ui.user
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.wavesoffood.R
 
-class MainActivity : AppCompatActivity() {
+class OnBoardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_on_boarding)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val tv_skip = findViewById<TextView>(R.id.btn_skip)
+
+        tv_skip.setOnClickListener{
+            val intent = Intent(applicationContext,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
