7f6130c7687d44b9132fd043049fe95475d5670a
ersurajrajput <thsurajsinghrajput@gmail.com>
2025-08-26 16:57:56 +0530

    choose your location ui done

---
+++ app/src/main/java/com/ersurajrajput/wavesoffood/ChooseLocationActivity.kt
@@ -0,0 +1,36 @@
+package com.ersurajrajput.wavesoffood
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import com.ersurajrajput.wavesoffood.databinding.ActivityChooseLocationBinding
+
+
+class ChooseLocationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChooseLocationBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityChooseLocationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var city = ArrayList<String>()
+        city.add("mathura")
+        city.add("agra")
+        city.add("noida")
+        city.add("gurugram")
+
+
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,city)
+
+        binding.locationAutoComplete.setAdapter(adapter)
+    }
+}
